43a9305e299aaaf500d57841ea462772b2ac9da7
Axel <axosar@gmail.com>
2017-06-30 18:07:36 -0500

    Array and Routing
    Testing Arrays and routing

--- js/app/app.config.js
+++ js/app/app.config.js
@@ -1,6 +1,25 @@
 'use strict'
 
 angular.module('try').
-  config(function(){
-    
+  config(
+    function(
+      $locationProvider,
+      $routeProvider
+    ){
+
+      $routeProvider.
+        when("/", {
+          template: "<blog-list></blog-list>"
+        }),
+        when("/blog/1", {
+          template: "<h1>Hi</h1>"
+        }).
+        when("/blog/2", {
+          template: "<blog-list></blog-list>"
+        }).
+        otherwise({
+          template: "Not Found"
+        })
+
+
   });
--- js/app/blog-list/blog-list.component.js
+++ js/app/blog-list/blog-list.component.js
@@ -5,6 +5,15 @@ angular.module('blogList').
       //template: "<div class=''><h1 class='new-class'>{{title}}</h1><button ng-Click='someClickTest()'>Click Me</button></div>",
       templateUrl: "/templates/blog-list.html",
       controller: function($scope){
+          var blogItems = [
+            {title: "Some title", id: 1, description: "This is a Book"},
+            {title: "Otro Titulo", id: 2, description: "Este es un libro"},
+            {title: "Autre Livre", id: 3, description: "Celui ci, un autre livre"},
+            {title: "Bookinteng", id: 4, description: "A bookentag in austamen"},
+          ]
+
+          $scope.items = blogItems
+
           $scope.title = 'Hi There!'
           $scope.clicks = 0
           $scope.someClickTest = function(){
